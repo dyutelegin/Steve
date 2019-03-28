6719347fad9249101f39805a08f01accbc254d6a
Dmitrii Telegin <46483682+dyutelegin@users.noreply.github.com>
2019-03-28 09:43:55 +0300

    Update Application.java

--- Application.java
+++ Application.java
@@ -5,8 +5,22 @@ public class Application {
     static String author = "Telegin Dmitrii";
     static  String version = "0.0.0";
     public static void main(String[] args){
-        System.out.println("Hello my name is " + APP_NAME);
-        System.out.println("my author's name is " + author);
-        System.out.println("my version name is " + version);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String command = reader.readLine();
+            if (!command.isEmpty()) {
+                if (command.equals("version")) {
+                    System.out.println("My version name is " + version);
+                }
+                if (command.equals("author") || command.equals("creator") || command.equals("father")) {
+                    System.out.println("My author's name is " + author);
+                }
+                if (command.matches("[A-Z?]")){
+                    System.out.println("I don't understand you, sorry");
+                }
+            }
+            else break;
+        }
+        reader.close();
     }
 }
