dbcf0bb66fa281819086ceb1f4a27786a8ce9c0f
Dmitrii Telegin <46483682+dyutelegin@users.noreply.github.com>
2019-03-28 08:58:18 +0300

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,12 @@
+package com.javacore.epam;
+
+public class Application {
+    static final String APP_NAME = "Steve";
+    static String author = "Telegin Dmitrii";
+    static  String version = "0.0.0";
+    public static void main(String[] args){
+        System.out.println("Hello my name is " + APP_NAME);
+        System.out.println("my author's name is " + author);
+        System.out.println("my version name is " + version);
+    }
+}
